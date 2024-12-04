58ba355c640c8e0b74743d6536b97cc3c304811f
Mayank Goel <mayank.goel447@gmail.com>
2024-12-04 21:15:26 +0530

    Toggling JS script done for mobile-navbar

--- scripts/mobile-navbar.js
+++ scripts/mobile-navbar.js
@@ -1,3 +1,13 @@
+let mobileNavbarOn = false;
+
 function showMobileNavbar() {
     const navLinks = document.querySelector(".nav-links");
+    if(mobileNavbarOn){
+        navLinks.style.display = "none";
+        mobileNavbarOn = false;
+    }
+    else{
+        navLinks.style.display = "block";
+        mobileNavbarOn = true;
+    }
 }
