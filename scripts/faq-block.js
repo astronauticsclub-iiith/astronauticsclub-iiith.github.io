2742307ea616762e093cd898d7039d33a7472891
Mayank Goel <mayank.goel447@gmail.com>
2024-12-07 02:54:08 +0530

    Added a read one question at a time functionality to FAQS

--- scripts/faq-block.js
+++ scripts/faq-block.js
@@ -1,5 +1,24 @@
 const bg_color = "#afadad";
 const active_color = "#eae5e5";
+const num_faqs = 8;
+
+function reset_faqs(){
+    let question, arrow, answer;
+
+    for (let i = 1; i <= num_faqs; i++) {
+        answer = document.getElementById("ans_"+i);
+        
+        if(answer.style.display == "block"){
+            arrow = document.getElementById("arrow_"+i);
+            question = document.getElementById("question_"+i);
+
+            answer.style.display = "none";
+            arrow.src = "../icons/down_arrow.png";
+            question.style.backgroundColor = bg_color;
+        }
+    }
+}
+
 
 function toggle_answer(id){
     let question = document.getElementById("question_"+id);
@@ -7,6 +26,7 @@ function toggle_answer(id){
     let answer = document.getElementById("ans_"+id);
 
     if (answer.style.display == "none" || answer.style.display == "") {
+        reset_faqs(); // only one answer can be active at a time
         answer.style.display = "block";
         arrow.src = "../icons/up_arrow.png";
         question.style.backgroundColor = active_color;
