c2fff57aff0acb1b5614c4db85e97b896e4bc778
Mayank Goel <mayank.goel447@gmail.com>
2024-12-18 00:26:52 +0530

    FAQ section code bugs removed

--- scripts/faq-block.js
+++ scripts/faq-block.js
@@ -1,39 +1,34 @@
-const bg_color = "#afadad";
-const active_color = "#eae5e5";
-const num_faqs = 8;
+let active_cell = -1000;
 
 function reset_faqs(){
-    let question, arrow, answer;
-
-    for (let i = 1; i <= num_faqs; i++) {
-        answer = document.getElementById("ans_"+i);
-        
-        if(answer.style.display == "block"){
-            arrow = document.getElementById("arrow_"+i);
-            question = document.getElementById("question_"+i);
-
-            answer.style.display = "none";
-            arrow.src = "../icons/down_arrow.png";
-            question.style.backgroundColor = bg_color;
-        }
-    }
+    if(active_cell <= 0) return;
+    let question = document.getElementById("question_"+active_cell);
+    question.classList.remove("faq-highlight");
+    question.parentElement.removeAttribute('open');
+    
+    let arrow = document.getElementById("arrow_"+active_cell);
+    arrow.classList.remove("fa-caret-up");
+    arrow.classList.add("fa-caret-down");
+    
+    active_cell = -1000;
 }
 
 
 function toggle_answer(id){
     let question = document.getElementById("question_"+id);
     let arrow = document.getElementById("arrow_"+id);
-    let answer = document.getElementById("ans_"+id);
 
-    if (answer.style.display == "none" || answer.style.display == "") {
+    if (active_cell != id){
         reset_faqs(); // only one answer can be active at a time
-        answer.style.display = "block";
-        arrow.src = "../icons/up_arrow.png";
-        question.style.backgroundColor = active_color;
+        arrow.classList.remove("fa-caret-down");
+        arrow.classList.add("fa-caret-up");
+        question.classList.add("faq-highlight");
+        active_cell = id;
     } 
     else {
-        answer.style.display = "none";
-        arrow.src = "../icons/down_arrow.png";
-        question.style.backgroundColor = bg_color;
+        question.classList.remove("faq-highlight");
+        arrow.classList.remove("fa-caret-up");
+        arrow.classList.add("fa-caret-down");
+        active_cell = -1000;
     }
 }
