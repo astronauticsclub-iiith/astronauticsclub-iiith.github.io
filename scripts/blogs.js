388000fabf40b9909fcdb71335a5e55f16453a62
Mayank Goel <mayank.goel447@gmail.com>
2024-12-25 03:43:24 +0530

    Made top blog circles dynamic

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,10 +1,11 @@
 let topBlogNum = 1;
-const numTopBlogs = 3;
+const numTopBlogs = 2;
 
 const topBlogSection = document.querySelector(".top-blog-section")
 const blogTitle = document.getElementById("top-blog-title")
 const blogDesc = document.getElementById("top-blog-description")
 const blogCategory = document.getElementById("top-blog-category")
+const blogDetails = document.querySelector('.blog-details-wrapper')
 
 const leftArrow = document.getElementById("slider-left-arrow");
 const rightArrow = document.getElementById("slider-right-arrow");
@@ -29,11 +30,22 @@ let blogTagList = [
     "Astronomy",
 ]
 
+///////////////// HELPER FUNCTIONS //////////////////
+function changeTopBlog(){
+    topBlogSection.style.backgroundImage = `url(${blogImageList[topBlogNum - 1]})`;
+    blogTitle.textContent = `${blogTitleList[topBlogNum - 1]}`;
+    blogDesc.textContent = `${blogDescriptionList[topBlogNum - 1]}`;
+    blogCategory.textContent = `${blogTagList[topBlogNum - 1]}`;
+    addCircleFill();
+}
+
 function checkArrowDisplay(){
     if(topBlogNum == 1){
         leftArrow.style.display = "none"
+        rightArrow.style.display = "block"
     }
     else if(topBlogNum == numTopBlogs){
+        leftArrow.style.display = "block"
         rightArrow.style.display = "none"
     }
     else{
@@ -42,24 +54,38 @@ function checkArrowDisplay(){
     }
 }
 
-function changeTopBlog(){
-    const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
-    topblogNumber.classList.add('filled')
-    topBlogSection.style.backgroundImage = `url(${blogImageList[topBlogNum - 1]})`;
-    blogTitle.textContent = `${blogTitleList[topBlogNum - 1]}`;
-    blogDesc.textContent = `${blogDescriptionList[topBlogNum - 1]}`;
-    blogCategory.textContent = `${blogTagList[topBlogNum - 1]}`;
+function createInitCircleList(){
+    const circleList = document.createElement('div')
+    circleList.classList.add('topblogCircle')
+    blogDetails.appendChild(circleList)
+
+    for(let i=0; i < numTopBlogs; i++){
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circleList.appendChild(circle)
+    }
 }
 
+function addCircleFill(){
+    const topblogNumber = document.querySelector(`.topblogCircle span:nth-child(${topBlogNum})`);
+    topblogNumber.classList.add('filled')
+}
+
+function removeCircleFill(){
+    const topblogNumber = document.querySelector(`.topblogCircle span:nth-child(${topBlogNum})`);
+    topblogNumber.classList.remove('filled')
+}
+
+
 document.addEventListener('DOMContentLoaded', ()=> {
-    checkArrowDisplay();
+    createInitCircleList();
     changeTopBlog();
+    checkArrowDisplay();
 })
 
 leftArrow.addEventListener('click', () => {
     if(topBlogNum > 1){
-        const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
-        topblogNumber.classList.remove('filled')
+        removeCircleFill()
         topBlogNum--;
         changeTopBlog();
         checkArrowDisplay();
@@ -68,8 +94,7 @@ leftArrow.addEventListener('click', () => {
 
 rightArrow.addEventListener('click', () => {
     if(topBlogNum < numTopBlogs){
-        const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
-        topblogNumber.classList.remove('filled')
+        removeCircleFill()
         topBlogNum++;
         changeTopBlog();
         checkArrowDisplay();
