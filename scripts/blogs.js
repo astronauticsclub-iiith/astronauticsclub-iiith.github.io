c139675f80fb6565db891f34cc0b49867dd759ed
Mayank Goel <mayank.goel447@gmail.com>
2024-12-25 03:05:09 +0530

    Top blog slider completed

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,23 +1,77 @@
-const backToTopButton = document.getElementById('back-to-top');
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    backToTopButton.style.display = 'block';
-  } else {
-    backToTopButton.style.display = 'none';
-  }
-});
-backToTopButton.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+let topBlogNum = 1;
+const numTopBlogs = 3;
 
-const searchBar = document.getElementById('search-bar');
-  const blogPosts = document.querySelectorAll('.blogpost-subcontainer');
+const topBlogSection = document.querySelector(".top-blog-section")
+const blogTitle = document.getElementById("top-blog-title")
+const blogDesc = document.getElementById("top-blog-description")
+const blogCategory = document.getElementById("top-blog-category")
 
-  searchBar.addEventListener('input', () => {
-    const searchText = searchBar.value.toLowerCase();
-    blogPosts.forEach(post => {
-      const title = post.querySelector('.blogpost-heading').innerText.toLowerCase();
-      const content = post.querySelector('.blogpost-content').innerText.toLowerCase();
-      post.style.display = (title.includes(searchText) || content.includes(searchText)) ? 'block' : 'none';
-    });
-  });
+const leftArrow = document.getElementById("slider-left-arrow");
+const rightArrow = document.getElementById("slider-right-arrow");
+
+let blogImageList = [
+    "../images/blog_1.jpg",
+    "../images/telescope_blog.jpg",
+]
+
+let blogTitleList = [
+    "Astronomy 101: Your First Steps into Astronomy and Astrophysics",
+    "A Beginner’s Guide to Telescopes",
+]
+
+let blogDescriptionList = [
+    "An approachable guide for curious minds to the wonders of astronomy",
+    "Explore the fascinating world of telescopes! Learn about their types, how they work, and essential tips for handling",
+]
+
+let blogTagList = [
+    "Astronomy",
+    "Astronomy",
+]
+
+function checkArrowDisplay(){
+    if(topBlogNum == 1){
+        leftArrow.style.display = "none"
+    }
+    else if(topBlogNum == numTopBlogs){
+        rightArrow.style.display = "none"
+    }
+    else{
+        leftArrow.style.display = "block"
+        rightArrow.style.display = "block"
+    }
+}
+
+function changeTopBlog(){
+    const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
+    topblogNumber.classList.add('filled')
+    topBlogSection.style.backgroundImage = `url(${blogImageList[topBlogNum - 1]})`;
+    blogTitle.textContent = `${blogTitleList[topBlogNum - 1]}`;
+    blogDesc.textContent = `${blogDescriptionList[topBlogNum - 1]}`;
+    blogCategory.textContent = `${blogTagList[topBlogNum - 1]}`;
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    checkArrowDisplay();
+    changeTopBlog();
+})
+
+leftArrow.addEventListener('click', () => {
+    if(topBlogNum > 1){
+        const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
+        topblogNumber.classList.remove('filled')
+        topBlogNum--;
+        changeTopBlog();
+        checkArrowDisplay();
+    }
+})
+
+rightArrow.addEventListener('click', () => {
+    if(topBlogNum < numTopBlogs){
+        const topblogNumber = document.querySelector(`#topblog-number span:nth-child(${topBlogNum})`);
+        topblogNumber.classList.remove('filled')
+        topBlogNum++;
+        changeTopBlog();
+        checkArrowDisplay();
+    }
+})
