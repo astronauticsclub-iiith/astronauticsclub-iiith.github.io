3e805a6e0e9336c0ee96c8577177cc4ad7dc8d4a
Mohit Singh <unignoramus@gmail.com>
2025-07-06 16:51:32 +0530

    Further reduce glitch interval

--- src/components/features/GlitchText.tsx
+++ src/components/features/GlitchText.tsx
@@ -21,7 +21,7 @@ const GlitchText: React.FC<GlitchTextProps> = ({ text, className }) => {
         }
       }
       setGlitchedText(newText);
-    }, 50);
+    }, 300);
 
     return () => clearInterval(interval);
   }, [text]);
