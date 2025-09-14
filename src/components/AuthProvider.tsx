50c7654fd07f2c9cd412709700f867eed66cacd3
Mayank Goel <mayank.goel447@gmail.com>
2025-09-15 04:25:44 +0530

    Added base path to Auth Provider

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -8,5 +8,9 @@ interface AuthProviderProps {
 }
 
 export default function AuthProvider({ children }: AuthProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath="/astronautics/api/auth">
+      {children}
+    </SessionProvider>
+  );
 }
