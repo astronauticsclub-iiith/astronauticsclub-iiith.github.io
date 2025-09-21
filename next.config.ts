ece1052c7805ef1e0c7689d00e5ef41232f23916
root <root@Astronautics.iiit.ac.in>
2025-09-21 18:57:42 +0000

    Minor changes to image upload config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",      
   output: 'standalone',
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'clubs.iiit.ac.in',
+        pathname: '/astronautics/uploads/**',  // include basePath here!
+      },
+    ],
+  },
 };
 
 
