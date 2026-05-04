6e439d5507f0a35cb88d0d646d6da1cb10caeb59
satkarjuneja <satkarjuneja@gmail.com>
2026-05-04 17:24:01 +0530

    done correctly

--- Docker-deployment/scripts/init-uploads.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script ensures that the uploads directory exists
-# Run this before starting the containers if needed
-
-mkdir -p ./public/uploads
-echo "Upload directory structure has been created."
