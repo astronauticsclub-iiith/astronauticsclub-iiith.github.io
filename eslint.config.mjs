61ceca185168a5735b0b35d15efa130b934493d4
Mayank Goel <mayank.goel447@gmail.com>
2026-05-18 01:13:36 +0530

    fix: Remove FlatCompat from eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
+import nextConfig from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+    ...nextConfig,
+    ...nextCoreWebVitals,
+    ...nextTypescript,
+    prettierConfig,
 
     {
         rules: {
