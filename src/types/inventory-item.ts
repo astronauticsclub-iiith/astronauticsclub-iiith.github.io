421fe3293bc6aec24ba025a8831aafe0b84662a1
Mayank Goel <mayank.goel447@gmail.com>
2026-05-21 17:16:40 +0530

    fix: Correct mismatched ts types, moongose schema

--- src/types/inventory-item.ts
+++ src/types/inventory-item.ts
@@ -2,10 +2,10 @@ export interface Inventory {
     id: string;
     name: string;
     image: string;
-    category: "astronomy" | "electronics" | "event" | "others";
+    category: "astronomy" | "electronics" | "events" | "others";
     description: string;
     year_of_purchase: number;
-    status: "working" | "repair" | "broken";
+    status: "working" | "needs repair" | "completely broken";
     isLent: boolean;
     borrower?: string;
     borrowed_date?: string;
